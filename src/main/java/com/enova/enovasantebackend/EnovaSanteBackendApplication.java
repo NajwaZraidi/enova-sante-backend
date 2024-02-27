7461cd44f3c60adcda20418e5388aacbfabf3762
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2024-02-27 11:02:28 +0100

    init project

---
+++ src/main/java/com/enova/enovasantebackend/EnovaSanteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enova.enovasantebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnovaSanteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnovaSanteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enova/enovasantebackend/EnovaSanteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enova.enovasantebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnovaSanteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
