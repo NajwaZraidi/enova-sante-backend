96f2ae65185f98999034fe03e9a13286d1a29e28
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2024-03-09 20:47:13 +0100

    fixed specifications endpoint empty request's body error

--- src/main/java/com/enova/enovasantebackend/repository/criteria/CategorieDocumentRequestCriteria.java
+++ src/main/java/com/enova/enovasantebackend/repository/criteria/CategorieDocumentRequestCriteria.java
@@ -3,9 +3,10 @@ package com.enova.enovasantebackend.repository.criteria;
 import lombok.Data;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Data
 public class CategorieDocumentRequestCriteria {
     private PageRequestCriteria pageRequestCriteria = new PageRequestCriteria();
-    private List<CategorieDocumentSearchCriteria> searchCriteriaList;
+    private List<CategorieDocumentSearchCriteria> searchCriteriaList = new ArrayList<CategorieDocumentSearchCriteria>();
 }
